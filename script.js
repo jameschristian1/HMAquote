8169aee8a1f32b9bc506f8c51318cfeaea00925b
jameschristian1 <james@jameschristian.com.au>
2026-05-11 17:02:06 +0930

    Update 260511_170206

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Replace this with your "Published as CSV" link from Google Sheets
 const SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTsQOS8r4GbYTOG_PBqeTNjTUBsvyURtrN2SqCw4lnoeeW7PvLdvcUqqIH0QOuDY8XBnLEjBiBJQI78/pub?output=csv';
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzKuoAJ1vRExVFym4EWKrkJLpOXwr_g8hCp6fAz_d4R2iFYstct5NFYRBAEf80rNB53UA/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyENnpusoly6B7eR2wRZomXmYKF3efbh_QcTtukGDpP1gYyBDHzwtM-TwPNYwpqERYoRg/exec';
 
 let airstripData = [];
 
